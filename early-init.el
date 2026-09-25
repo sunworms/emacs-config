7f2a2a74dc125571e48c79cae47bf3faf2cab38d
Sunny <sunnybhowmick0310@gmail.com>
2026-09-25 21:51:46 +0530

    fix for gtk emacs not loading theme

--- early-init.el
+++ early-init.el
@@ -38,7 +38,8 @@
 ;; Give the frame basic coloring while waiting for the theme to load. The main
 ;; purpose of this is to not blind me when it's dark by flashing a screen full
 ;; of white. These colors are from doom-one.
-(set-face-attribute 'default nil :background "#282c34" :foreground "#bbc2cf")
+(push '(background-color . "#1e1e2e") initial-frame-alist)
+(push '(foreground-color . "#cdd6f4") initial-frame-alist)
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
